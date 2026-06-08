b2506fe214e07d3e458e40cba32e211264a2c8a3
MusaRehman <workforrehman@gmail.com>
2026-06-09 02:42:49 +0500

    added code for queues

--- src/models/index.js
+++ src/models/index.js
@@ -13,6 +13,66 @@ const InventoryMovement = createInventoryMovements(sequelize);
 const Product = createProducts(sequelize);
 const OutboxEvent = createOutboxEvents(sequelize);
 
+User.hasMany(Order, {
+  foreignKey: "user_id",
+  onDelete: "RESTRICT",
+  onUpdate: "CASCADE",
+});
+
+Order.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "RESTRICT",
+  onUpdate: "CASCADE",
+});
+
+Order.hasMany(OrderItem, {
+  foreignKey: "order_id",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+
+OrderItem.belongsTo(Order, {
+  foreignKey: "order_id",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+
+Product.hasMany(OrderItem, {
+  foreignKey: "product_id",
+  onDelete: "RESTRICT",
+  onUpdate: "CASCADE",
+});
+
+OrderItem.belongsTo(Product, {
+  foreignKey: "product_id",
+  onDelete: "RESTRICT",
+  onUpdate: "CASCADE",
+});
+
+Product.hasMany(InventoryMovement, {
+  foreignKey: "product_id",
+  onDelete: "RESTRICT",
+  onUpdate: "CASCADE",
+});
+
+InventoryMovement.belongsTo(Product, {
+  foreignKey: "product_id",
+  onDelete: "RESTRICT",
+  onUpdate: "CASCADE",
+});
+
+Order.hasMany(InventoryMovement, {
+  foreignKey: "order_id",
+  onDelete: "RESTRICT",
+  onUpdate: "CASCADE",
+});
+
+InventoryMovement.belongsTo(Order, {
+  foreignKey: "order_id",
+  onDelete: "RESTRICT",
+  onUpdate: "CASCADE",
+});
+
 export const db = {
   sequelize,
   User,
